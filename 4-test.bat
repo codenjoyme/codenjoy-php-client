f5e4631f3f6d7a83aa36c8ac483954e7a140b00e
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-08 03:02:16 +0200

    [clients][php][python][bat] Fixed color output.

--- 4-test.bat
+++ 4-test.bat
@@ -4,25 +4,33 @@ echo off
 call lib :color Starting php tests...
 echo on
 
-call lib :sep
+echo off
+call lib :color Engine tests...
+echo on
 
 cd tests\engine
 call %PHPUNIT% --no-configuration %CD%\
 cd %ROOT%
 
-call lib :sep
+echo off
+call lib :color Clifford tests...
+echo on
 
 cd tests\games\clifford
 call %PHPUNIT% --no-configuration %CD%\
 cd %ROOT%
 
-call lib :sep
+echo off
+call lib :color Mollymage tests...
+echo on
 
 cd tests\games\mollymage
 call %PHPUNIT% --no-configuration %CD%\
 cd %ROOT%
 
-call lib :sep
+echo off
+call lib :color Sample tests...
+echo on
 
 cd tests\games\sample
 call %PHPUNIT% --no-configuration %CD%\
