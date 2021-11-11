8f17b859e3b136925a994f562766768d362e301f
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-11 09:21:01 +0200

    [clients][php][games][all] Added element files for games.

---
+++ games/icancode/Element.php
@@ -0,0 +1,135 @@
+<?php
+
+###
+# #%L
+# Codenjoy - it's a dojo-like platform from developers to developers.
+# %%
+# Copyright (C) 2021 Codenjoy
+# %%
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.html>.
+# #L%
+###
+
+namespace Icancode;
+
+abstract class Element
+{
+    static array $elements = array(
+
+        "EMPTY" => '-',
+
+        "FLOOR" => '.',
+
+        "ANGLE_IN_LEFT" => '╔',
+
+        "WALL_FRONT" => '═',
+
+        "ANGLE_IN_RIGHT" => '┐',
+
+        "WALL_RIGHT" => '│',
+
+        "ANGLE_BACK_RIGHT" => '┘',
+
+        "WALL_BACK" => '─',
+
+        "ANGLE_BACK_LEFT" => '└',
+
+        "WALL_LEFT" => '║',
+
+        "WALL_BACK_ANGLE_LEFT" => '┌',
+
+        "WALL_BACK_ANGLE_RIGHT" => '╗',
+
+        "ANGLE_OUT_RIGHT" => '╝',
+
+        "ANGLE_OUT_LEFT" => '╚',
+
+        "SPACE" => ' ',
+
+        "LASER_MACHINE_CHARGING_LEFT" => '˂',
+
+        "LASER_MACHINE_CHARGING_RIGHT" => '˃',
+
+        "LASER_MACHINE_CHARGING_UP" => '˄',
+
+        "LASER_MACHINE_CHARGING_DOWN" => '˅',
+
+        "LASER_MACHINE_READY_LEFT" => '◄',
+
+        "LASER_MACHINE_READY_RIGHT" => '►',
+
+        "LASER_MACHINE_READY_UP" => '▲',
+
+        "LASER_MACHINE_READY_DOWN" => '▼',
+
+        "START" => 'S',
+
+        "EXIT" => 'E',
+
+        "HOLE" => 'O',
+
+        "BOX" => 'B',
+
+        "ZOMBIE_START" => 'Z',
+
+        "GOLD" => '$',
+
+        "UNSTOPPABLE_LASER_PERK" => 'l',
+
+        "DEATH_RAY_PERK" => 'r',
+
+        "UNLIMITED_FIRE_PERK" => 'f',
+
+        "FIRE_PERK" => 'a',
+
+        "JUMP_PERK" => 'j',
+
+        "MOVE_BOXES_PERK" => 'm',
+
+        "ROBO" => '☺',
+
+        "ROBO_FALLING" => 'o',
+
+        "ROBO_FLYING" => '*',
+
+        "ROBO_LASER" => '☻',
+
+        "ROBO_OTHER" => 'X',
+
+        "ROBO_OTHER_FALLING" => 'x',
+
+        "ROBO_OTHER_FLYING" => '^',
+
+        "ROBO_OTHER_LASER" => '&',
+
+        "LASER_LEFT" => '←',
+
+        "LASER_RIGHT" => '→',
+
+        "LASER_UP" => '↑',
+
+        "LASER_DOWN" => '↓',
+
+        "FEMALE_ZOMBIE" => '♀',
+
+        "MALE_ZOMBIE" => '♂',
+
+        "ZOMBIE_DIE" => '✝',
+
+        "FOG" => 'F',
+
+        "BACKGROUND" => 'G'
+    );
+}
---
+++ games/minesweeper/Element.php
@@ -0,0 +1,95 @@
+<?php
+
+###
+# #%L
+# Codenjoy - it's a dojo-like platform from developers to developers.
+# %%
+# Copyright (C) 2021 Codenjoy
+# %%
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.html>.
+# #L%
+###
+
+namespace Minesweeper;
+
+abstract class Element
+{
+    static array $elements = array(
+
+            # Сапер взорвался на бомбе.
+
+        "BANG" => 'Ѡ',
+
+            # .
+
+        "HERE_IS_BOMB" => '☻',
+
+            # Сапер.
+
+        "DETECTOR" => '☺',
+
+            # Флажок, указывающий что тут вероятно бомба.
+
+        "FLAG" => '‼',
+
+            # Неизведанный участок поля.
+
+        "HIDDEN" => '*',
+
+            # Граница поля или препятствие для перемещения.
+
+        "BORDER" => '☼',
+
+            # Уничтоженная бомба.
+
+        "DESTROYED_BOMB" => 'x',
+
+            # Вокруг этой клеточки нет бомб.
+
+        "NONE" => ' ',
+
+            # Вокруг этой клеточки одна бомба.
+
+        "ONE_MINE" => '1',
+
+            # Вокруг этой клеточки две бомбы.
+
+        "TWO_MINES" => '2',
+
+            # Вокруг этой клеточки три бомбы.
+
+        "THREE_MINES" => '3',
+
+            # Вокруг этой клеточки четыре бомбы.
+
+        "FOUR_MINES" => '4',
+
+            # Вокруг этой клеточки пять бомб.
+
+        "FIVE_MINES" => '5',
+
+            # Вокруг этой клеточки шесть бомб.
+
+        "SIX_MINES" => '6',
+
+            # Вокруг этой клеточки семь бомб.
+
+        "SEVEN_MINES" => '7',
+
+            # Вокруг этой клеточки восемь бомб.
+
+        "EIGHT_MINES" => '8'
+    );
+}
