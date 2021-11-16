ebe478cc9f46165c9b4e869c211c3537d0151ddf
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-17 00:20:21 +0200

    [clients][php][clifford] Fixed compilation error. Updated versions.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once "engine/classes.php";
 require_once "games/mollymage/classes.php";
 require_once "games/clifford/classes.php";
 
-$game = "clifford";
+$game = "mollymage";
 $url = "http://localhost:8080/codenjoy-contest/board/player/0?code=000000000000";
 if ($argc == 3) {
     $game = $argv[1];
@@ -20,7 +20,7 @@ function determineGameSolver(string $game): GameSolver
 {
     return match ($game) {
         "mollymage" => new MollyMage\Solver(),
-        "clifford" => new DetectiveClifford\Solver(),
+        "clifford" => new Clifford\Solver(),
         default => throw new InvalidArgumentException("no solver for game: " . $game),
     };
 }
