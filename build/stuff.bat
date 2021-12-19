e1f4d9112d3f7d3792badec7f14e5532e1ad7cc6
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-12-19 16:49:30 +0200

    [clients][all][build][batch] Used improvements from portable windows.

--- build/stuff.bat
+++ build/stuff.bat
@@ -1,6 +1,9 @@
 @echo off
 
-call run :init_colors
+if "%RUN%"=="" set RUN=%CD%\run
+if "%STUFF%"=="" set STUFF=%CD%\stuff
+
+call %RUN% :init_colors
 
 :check_run_mode
     if "%*"=="" (       
@@ -12,8 +15,8 @@ call run :init_colors
 
 :run_executable
     rem run stuff.bat as executable script
-    call run :color ‘%CL_INFO%‘ ‘This is not executable script. Please use 'run.bat' only.‘
-    call run :ask   
+    call %RUN% :color ‘%CL_INFO%‘ ‘This is not executable script. Please use 'run.bat' only.‘
+    call %RUN% :ask   
     goto :eof
 
 :run_library
@@ -32,8 +35,8 @@ call run :init_colors
     set PHPUNIT=%ROOT%\vendor\bin\phpunit.bat
 
     echo Language environment variables
-    call run :color ‘%CL_INFO%‘ ‘PATH=%PATH%‘
-    call run :color ‘%CL_INFO%‘ ‘PHP_HOME=%PHP_HOME%‘
+    call %RUN% :color ‘%CL_INFO%‘ ‘PATH=%PATH%‘
+    call %RUN% :color ‘%CL_INFO%‘ ‘PHP_HOME=%PHP_HOME%‘
 
     set ARCH_URL=https://windows.php.net/downloads/releases/archives/php-8.0.8-nts-Win32-vs16-x64.zip
     set ARCH_FOLDER=
@@ -42,22 +45,22 @@ call run :init_colors
 
 :install
     if exist %TOOLS%\..\vendor (
-        call run :eval_echo ‘rd /S /Q %TOOLS%\..\vendor‘
+        call %RUN% :eval_echo ‘rd /S /Q %TOOLS%\..\vendor‘
     )
-    call run :install php %ARCH_URL% %ARCH_FOLDER%
+    call %RUN% :install php %ARCH_URL% %ARCH_FOLDER%
     if exist %TOOLS%\composer.phar (
-        call run :eval_echo ‘del /Q %TOOLS%\composer.phar‘
+        call %RUN% :eval_echo ‘del /Q %TOOLS%\composer.phar‘
     )
-    call run :download_file ‘%ARCH_PHP_COMPOSER%‘ ‘%TOOLS%\..\composer.phar‘
-    call run :eval_echo ‘xcopy /y %TOOLS%\php.ini %PHP_HOME%\‘
+    call %RUN% :download_file ‘%ARCH_PHP_COMPOSER%‘ ‘%TOOLS%\..\composer.phar‘
+    call %RUN% :eval_echo ‘xcopy /y %TOOLS%\php.ini %PHP_HOME%\‘
     goto :eof
 
 :version
-    call run :eval_echo_color ‘%PHP% -v‘
+    call %RUN% :eval_echo_color ‘%PHP% -v‘
     goto :eof
 
 :build
-    call run :eval_echo ‘%PHP% composer.phar u‘
+    call %RUN% :eval_echo ‘%PHP% composer.phar u‘
     goto :eof
 
 :test    
@@ -81,9 +84,9 @@ call run :init_colors
     call %ROOT%\run :eval_echo ‘call %PHPUNIT% --no-configuration %CD%\‘
     call %ROOT%\run :eval_echo ‘cd %ROOT%‘
 
-    call run :sep
+    call %RUN% :sep
     goto :eof
 
 :run
-    call run :eval_echo ‘%PHP% index.php %GAME_TO_RUN% %SERVER_URL%‘
+    call %RUN% :eval_echo ‘%PHP% index.php %GAME_TO_RUN% %SERVER_URL%‘
     goto :eof
