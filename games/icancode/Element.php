ee996253ee1b50b76402cee1559c1bb52ff0af4e
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-02-15 16:18:53 +0200

    [icancode][clients][all][elements-generator] Generate all clients for icancode.

--- games/icancode/Element.php
+++ games/icancode/Element.php
@@ -28,107 +28,211 @@ abstract class Element
 {
     static array $elements = array(
 
+            # Empty space (on layer 2) where player can go.
+
         "EMPTY" => '-',
+
+            # Empty space (on layer 1) where player can go.
 
         "FLOOR" => '.',
 
+            # Wall (on layer 1).
+
         "ANGLE_IN_LEFT" => '╔',
+
+            # Wall (on layer 1).
 
         "WALL_FRONT" => '═',
 
+            # Wall (on layer 1).
+
         "ANGLE_IN_RIGHT" => '┐',
+
+            # Wall (on layer 1).
 
         "WALL_RIGHT" => '│',
 
+            # Wall (on layer 1).
+
         "ANGLE_BACK_RIGHT" => '┘',
+
+            # Wall (on layer 1).
 
         "WALL_BACK" => '─',
 
+            # Wall (on layer 1).
+
         "ANGLE_BACK_LEFT" => '└',
+
+            # Wall (on layer 1).
 
         "WALL_LEFT" => '║',
 
+            # Wall (on layer 1).
+
         "WALL_BACK_ANGLE_LEFT" => '┌',
+
+            # Wall (on layer 1).
 
         "WALL_BACK_ANGLE_RIGHT" => '╗',
 
+            # Wall (on layer 1).
+
         "ANGLE_OUT_RIGHT" => '╝',
+
+            # Wall (on layer 1).
 
         "ANGLE_OUT_LEFT" => '╚',
 
+            # Wall (on layer 1).
+
         "SPACE" => ' ',
+
+            # Charging laser machine (on layer 1) pointing left.
 
         "LASER_MACHINE_CHARGING_LEFT" => '˂',
 
+            # Charging laser machine (on layer 1) pointing right.
+
         "LASER_MACHINE_CHARGING_RIGHT" => '˃',
+
+            # Charging laser machine (on layer 1) pointing up.
 
         "LASER_MACHINE_CHARGING_UP" => '˄',
 
+            # Charging laser machine (on layer 1) pointing down.
+
         "LASER_MACHINE_CHARGING_DOWN" => '˅',
+
+            # Charged laser machine (on layer 1) pointing left.
 
         "LASER_MACHINE_READY_LEFT" => '◄',
 
+            # Charged laser machine (on layer 1) pointing right.
+
         "LASER_MACHINE_READY_RIGHT" => '►',
+
+            # Charged laser machine (on layer 1) pointing up.
 
         "LASER_MACHINE_READY_UP" => '▲',
 
+            # Charged laser machine (on layer 1) pointing down.
+
         "LASER_MACHINE_READY_DOWN" => '▼',
+
+            # Level start spot (on layer 1).
 
         "START" => 'S',
 
+            # Level exit spot (on layer 1).
+
         "EXIT" => 'E',
+
+            # Hole (on layer 1).
 
         "HOLE" => 'O',
 
+            # Box (on layer 2) that can be moved and jumped over.
+
         "BOX" => 'B',
+
+            # Zombie start spot (on layer 1).
 
         "ZOMBIE_START" => 'Z',
 
+            # Gold (on layer 1).
+
         "GOLD" => '$',
+
+            # Unstoppable laser perk (on layer 1).
 
         "UNSTOPPABLE_LASER_PERK" => 'l',
 
+            # Death ray perk (on layer 1).
+
         "DEATH_RAY_PERK" => 'r',
+
+            # Unlimited fire perk (on layer 1).
 
         "UNLIMITED_FIRE_PERK" => 'f',
 
+            # Fire perk (on layer 1).
+
         "FIRE_PERK" => 'a',
+
+            # Jump perk (on layer 1).
 
         "JUMP_PERK" => 'j',
 
+            # Move boxes perk (on layer 1).
+
         "MOVE_BOXES_PERK" => 'm',
+
+            # Your hero (on layer 2).
 
         "ROBO" => '☺',
 
+            # Your hero (on layer 2) falls into a hole.
+
         "ROBO_FALLING" => 'o',
+
+            # Your hero (on layer 3) falling.
 
         "ROBO_FLYING" => '*',
 
+            # Your hero (on layer 2) died from a laser.
+
         "ROBO_LASER" => '☻',
+
+            # Other hero (on layer 2).
 
         "ROBO_OTHER" => 'X',
 
+            # Other hero (on layer 2) falls into a hole.
+
         "ROBO_OTHER_FALLING" => 'x',
+
+            # Other hero (on layer 3) falling.
 
         "ROBO_OTHER_FLYING" => '^',
 
+            # Other hero (on layer 2) died from a laser.
+
         "ROBO_OTHER_LASER" => '&',
+
+            # Laser (on layer 2) flies to the left.
 
         "LASER_LEFT" => '←',
 
+            # Laser (on layer 2) flies to the right.
+
         "LASER_RIGHT" => '→',
+
+            # Laser (on layer 2) flies to the up.
 
         "LASER_UP" => '↑',
 
+            # Laser (on layer 2) flies to the down.
+
         "LASER_DOWN" => '↓',
+
+            # Female zombie (on layer 2).
 
         "FEMALE_ZOMBIE" => '♀',
 
+            # Male zombie (on layer 2).
+
         "MALE_ZOMBIE" => '♂',
+
+            # Zombie dies (on layer 2).
 
         "ZOMBIE_DIE" => '✝',
 
+            # Fog of war system layer (on layer 1).
+
         "FOG" => 'F',
+
+            # Background system layer (on layer 2).
 
         "BACKGROUND" => 'G'
     );
